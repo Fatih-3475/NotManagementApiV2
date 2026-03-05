24ed428ed6efb19caa0acc54fd2dd197880c98c3
Fatih Korkmaz <fatih.korkmaz3475@gmail.com>
2026-03-05 15:00:50 +0300

    Database-connection

---
+++ Proje.v1/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proje.v1.Models;
+
+namespace Proje.v1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<Note>Notes { get; set; }
+    }
+}
